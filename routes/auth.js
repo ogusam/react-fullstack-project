86e4847e774b9cba1c1ea0e30e23a80dc03de3e7
ogusam <ogusam32@gmail.com>
2023-03-14 16:37:36 +0100

    second commit

---
+++ routes/auth.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { base } = require('../models/User');
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require('bcryptjs');
+const { hash } = require('bcrypt');
+
+router.get("/test", (req, res) =>{
+    res.send("Auth route working");
+});
+
+//@route Post/api/auth/register
+// discription Create a new user
+
+router.post("/register", async (req, res) =>{
+    try{
+        //hash password
+        const hasedPassword = await bcrypt.hash(req.body.password, 12);
+        const newUser = new User({
+            email: req.body.email,
+            password: hashedPassword,
+            name: req.body.name
+        })
+        //save user to data base
+        const savedUser = await newUser.save();
+    } catch(err){
+        console.log(err);
+        res.status(500).send(err.message);
+    }
+})
+module.exports= router;
--- server.js
+++ server.js
@@ -1,23 +1,52 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
+const authRoute = require("./routes/auth");
+
+const uri="mongodb+srv://ogusam:Password12345@cluster0.p4hxmas.mongodb.net/?retryWrites=true&w=majority"
+
+
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get("/", (req, res) =>{
+app.get("/api", (req, res) =>{
     res.send("Fullstack React Project");
-})
+});
 
+/*
 app.post("/name", (req, res) =>{
     if(req.body.name) {
         return res.json({name:req.body.name}) 
     }else{
-        return res.status(400).json({error:"no name provided"})
+        return res.status(400).json({error:"no name provided "})
     }
-})
+});
+*/
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server running on port ${process.env.PORT}`);
-})
+app.use("/api/auth", authRoute);
+
+    async function connect(){
+        try{
+            await mongoose.connect(uri);
+            console.log('connected to data base');
+        } catch(error){
+            console.error(error);
+        }
+    }
+    connect();
+/*
+ mongoose.connect(process.env.URI).then(()=>{
+    console.Console.log("connected to data base");
+
+ });
+
+ */
+    app.listen(process.env.PORT, ()=>{
+        console.log(`Server running on port ${process.env.PORT}`);
+    });
+
+
